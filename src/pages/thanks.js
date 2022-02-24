d09a4a07059456cd140e81494f2cf23e1cf93a7a
Emilio Ziniades <emilioziniades@protonmail.com>
2022-02-25 00:28:20 +0200

    updated thanks page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import HeroShort from "../components/heroShort"
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 
 export default function() {
     return (
