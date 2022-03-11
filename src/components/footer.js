310e8b900d024a8b8fccf9c19c54e4c272d8abdf
Emilio Ziniades <emilioziniades@protonmail.com>
2022-03-12 01:44:18 +0200

    added styles: global, layout, header, footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,37 +1,31 @@
 import React from "react"
+import { Link } from "gatsby"
 import styled from "styled-components"
 
-// const StyledFooter = styled.footer`
-//   width: 100%;
-//   height: 5.25rem;
-//   margin: 0 auto;
-//   padding: 0 2.5rem;
-//   background: black;
-//   color: white;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   font-size: 0.8rem;
-//   text-transform: uppercase;
-//   letter-spacing: +1px;
-//   font-weight: 700;
-// `
-
-const StyledFooter = styled.footer``
+const StyledFooter = styled.footer`
+  width: 100%;
+  height: 5.25rem;
+  margin: 0 auto;
+  padding: 0 2.5rem;
+  background: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  text-transform: uppercase;
+  letter-spacing: +1px;
+  font-weight: 700;
+`
 
 const Footer = () => (
   <StyledFooter>
-    <a
-      href=" mailto:emilioziniades@gmail.com "
-      target="_blank"
-      rel="nofollow noopener noreferrer"
-      aria-label="External Link"
-    >
+    <Link to="/contact" aria-label="Contact">
       <span role="img" aria-label="emoji">
         📬
       </span>{" "}
-      Drop me an email
-    </a>
+      Get in touch
+    </Link>
   </StyledFooter>
 )
 
--- src/components/header.js
+++ src/components/header.js
@@ -3,30 +3,67 @@ import { Link } from "gatsby"
 import { Helmet } from "react-helmet"
 import styled from "styled-components"
 
-// const StyledHeader = styled.header`
-//   width: 100%;
-//   max-width: 62.5rem;
-//   height: 6.25rem;
-//   margin: 0 auto;
-//   padding: 0 2.5rem;
-//   background: inherit;
-//   display: flex;
-//   justify-content: flex-start;
-//   align-items: center;
-// `
+const StyledHeader = styled.header`
+  width: 100%;
+  max-width: 62.5rem;
+  height: 6.25rem;
+  margin: 0 auto;
+  padding: 0 2.5rem;
+  background: inherit;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
 
-const StyledHeader = styled.header``
+const StyledLogo = styled.div`
+  font-size: 2rem;
+  font-weight: 900;
+  color: black;
+  margin: 1rem;
+  padding: 1rem;
+`
 
-// const StyledLogo = styled.div`
-//   font-size: 2rem;
-//   font-weight: 900;
-//   color: black;
-// `
-//
+const StyledNav = styled.nav`
+  a {
+    text-decoration: underline;
+    font-size: 1.5rem;
+    padding: 0.25rem 0.75rem;
+    font-weight: 700;
+  }
+  ul {
+    list-style-type: none;
+    display: float;
+  }
+  li {
+    padding: 0.25rem 0.5rem;
+  }
+  .contact {
+    margin: 0 2rem;
+  }
 
-const StyledNav = styled.nav``
-
-const StyledLogo = styled.div``
+  .contact-link {
+    border: 0.15rem solid black;
+    padding: 0.25rem 0.75rem;
+    text-decoration: none;
+  }
+  .active-code {
+    box-shadow: inset 0 -2.5rem 0 #adebce;
+    text-decoration: none;
+  }
+  .active-teach {
+    box-shadow: inset 0 -2.5rem 0 #e69eab;
+    text-decoration: none;
+  }
+  .active-write {
+    box-shadow: inset 0 -2.5rem 0 #becfee;
+    text-decoration: none;
+  }
+  .active-contact {
+    box-shadow: inset 0 -2.5rem 0 #ffdb70;
+    border: none;
+  }
+`
 
 const Header = () => {
   return (
@@ -42,22 +79,39 @@ const Header = () => {
       <StyledNav>
         <ul>
           <li>
-            <Link to="/teach" aria-label="teach-page">
+            <Link
+              to="/teach"
+              aria-label="teach-page"
+              activeClassName="active-teach "
+            >
               Teach
             </Link>
           </li>
           <li>
-            <Link to="/write" aria-label="write-page">
+            <Link
+              to="/write"
+              aria-label="write-page"
+              activeClassName="active-write "
+            >
               Write
             </Link>
           </li>
           <li>
-            <Link to="/code" aria-label="code-page">
+            <Link
+              to="/code"
+              aria-label="code-page"
+              activeClassName="active-code "
+            >
               Code
             </Link>
           </li>
-          <li>
-            <Link to="/contact" aria-label="contact-page">
+          <li className="contact">
+            <Link
+              className="contact-link"
+              to="/contact"
+              aria-label="contact-page"
+              activeClassName="active-contact "
+            >
               Contact
             </Link>
           </li>
