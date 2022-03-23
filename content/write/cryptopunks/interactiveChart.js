890c28b3906d437eb3982501252bf54d0284ccab
Emilio Ziniades <emilioziniades@protonmail.com>
2022-03-23 16:23:13 +0200

    added interactive chart to cryptopunks

--- content/write/cryptopunks/interactiveChart.js
+++ content/write/cryptopunks/interactiveChart.js
@@ -1,82 +1,106 @@
-import React from "react"
-import { Line } from "react-chartjs-2"
+import React, { useState } from "react"
+import Form from "react-bootstrap/Form"
+import Chart from "chart.js/auto"
+import { Scatter } from "react-chartjs-2"
 
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js"
+const getData = filename => {
+  const rawData = require(filename)
+  const myData = []
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-)
+  for (let i = 0; i < rawData.length; i++) {
+    var points = []
+    for (let j = 0; j < rawData[i].balances.length; j++) {
+      points.push({ x: rawData[i].x[j], y: rawData[i].y[j] })
+    }
+    var current = {
+      date: rawData[i].date,
+      gini: rawData[i].gini,
+      colour: rawData[i].colour,
+      points: points,
+    }
+    myData.push(current)
+    points = []
+  }
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top",
-    },
-    title: {
-      display: true,
-      text: "Figure 4: Distribution of ETH Value of Punks over time (interactive)",
-    },
-  },
-}
-
-const labels = ["January", "February"]
-
-const balances = require("./balances_eth_20.json")
-console.log(balances)
-
-// export const data = {
-//   labels,
-//   datasets: [
-//     {
-//       label: "Dataset 1",
-//       data: [100, 120],
-//       borderColor: "rgb(255, 99, 132)",
-//       backgroundColor: "rgba(255, 99, 132, 0.5)",
-//     },
-//     {
-//       label: "Dataset 2",
-//       data: [356, 700],
-//       borderColor: "rgb(53, 162, 235)",
-//       backgroundColor: "rgba(53, 162, 235, 0.5)",
-//     },
-//   ],
-// }
-
-console.log(balances[1].balances)
-export const data = {
-  labels: [0, 1, 2, 3, 4, 100],
-  datasets: [
-    {
-      label: "Test",
-      data: balances[1].balances,
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
+  return myData
 }
 
 const InteractiveChart = () => {
+  const myData = getData("./balances_interactive.json")
+  const [time, setTime] = useState(0)
+  const data = {
+    datasets: [
+      {
+        data: myData[time].points,
+        label: `${myData[time].date} (n = ${
+          myData[time].points.length
+        }, gini = ${myData[time].gini.toFixed(3)})`,
+        backgroundColor: myData[time].colour,
+      },
+    ],
+  }
+
+  Chart.defaults.font.size = 18
+  const options = {
+    showLine: true,
+    animation: false,
+    aspectRatio: 1.3,
+    scales: {
+      y: {
+        beginAtZero: true,
+        max: 1,
+        title: {
+          display: true,
+          text: "Cumulative Percent of ETH Value of Punks",
+          ticks: {
+            font: {
+              size: 100000,
+            },
+          },
+        },
+      },
+      x: {
+        beginAtZero: true,
+        max: 1,
+        title: {
+          display: true,
+          text: "Cumulative Percent of Addresses",
+        },
+      },
+    },
+    elements: {
+      point: {
+        radius: 0,
+      },
+      line: {
+        borderWidth: 4,
+        borderColor: myData[time].colour,
+      },
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: "Figure 4: Interactive Distribution of ETH Value of Punks",
+      },
+      tooltip: { enabled: false },
+    },
+  }
+
   return (
-    <div>
-      {" "}
-      I exist!!! <Line options={options} data={data} />
-    </div>
+    <>
+      <Scatter options={options} data={data} />
+      <Form className="my-3">
+        <Form.Group controlId="formBasicRange">
+          <Form.Range
+            value={time}
+            min={0}
+            max={myData.length - 1}
+            onChange={event => setTime(event.target.value)}
+          />
+        </Form.Group>
+      </Form>
+      <br />
+    </>
   )
 }
 
